9e97e2af142075141c863bf1af9310a3b7b3023d
rokayaraka <rokayaraka251@gmail.com>
2026-04-17 22:30:24 +0600

    list view

--- lib/presentation/screens/reciepie_item.dart
+++ lib/presentation/screens/reciepie_item.dart
@@ -20,25 +20,40 @@ class _ReciepieItemState extends State<ReciepieItem> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.reciepe.description ?? "No description available",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: const Color.fromARGB(255, 0, 0, 0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.reciepe.description ?? "No description available",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Ingredients",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            ...?widget.reciepe.ingredients?.map((ingredient) => Text("- $ingredient")),
-          ],
+              SizedBox(height: 20),
+              
+              Text(
+                "Ingredients",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+                SizedBox(height: 10),
+              SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    "https://img.freepik.com/free-vector/food-ingredients-recipe-ravioli-hand-drawn-vector-illustration_1284-42012.jpg?semt=ais_hybrid&w=740&q=80",
+                    fit: BoxFit.cover,
+                    )),
+              ),
+              SizedBox(height: 20),
+              ...?widget.reciepe.ingredients?.map((ingredient) => Text("- $ingredient")),
+            ],
+          ),
         ),
       ),
     );
